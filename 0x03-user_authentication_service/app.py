0dd758304f7067c40d0c2fbafe96fdbc228e95d8
userfiona <fionaramaesela.com>
2024-02-21 10:28:09 +0000

    how to add authentication in to your app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -29,25 +29,7 @@ def register():
        password = request.form['password']
         email = request.form['email']
 
-         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM user WHERE email = % s',
-                       (email, ))
-        account = cursor.fetchone()
-        if account:
-            message = 'Account already exists !'
-        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
-            message = 'Invalid email address !'
-        elif not userName or not password or not email:
-            message = 'Please fill out the form !'
-        else:
-            cursor.execute(
-                'INSERT INTO user VALUES (NULL, % s, % s, % s)',
-              (userName, email, password, ))
-            mysql.connection.commit()
-            message = 'You have successfully registered !'
-    elif request.method == 'POST':
-        message = 'Please fill out the form !'
-    return render_template('register.html', message=message)
+        
 @app.route('/')
 @app.route('/login', methods=['GET', 'POST'])
 def login():
