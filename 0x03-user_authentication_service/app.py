6a888379a960bb33a656be18d4240b5af751339f
userfiona <fionaramaesela.com>
2024-02-21 11:15:36 +0000

    how to add authentication in to your app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -36,6 +36,9 @@ def register():
 # login function verify username and password 
 def login():      
    error = None
+
+   email = request.form.get("email")
+   password = request.form.get("password")
      
    if request.method == 'POST': 
       if request.form['username'] != 'admin' or \ 
@@ -47,6 +50,25 @@ def login():
          flash('You were successfully logged in')  
          return redirect(url_for('index')) 
    return render_template('login.html', error = error) 
+
+@app.route("/sessions", methods=["POST"])
+def login():
+    """ Login endpoint
+        Form fields:
+            - email
+            - password
+        Return:
+            - user email and login message JSON represented
+            - 401 if credential are invalid
+    """
+    email = request.form.get("email")
+    password = request.form.get("password")
+    if not AUTH.valid_login(email, password):
+        abort(401)
+    session_id = AUTH.create_session(email)
+    response = jsonify({"email": email, "message": "logged in"})
+    response.set_cookie("session_id", session_id)
+    return response
 
  # Make function for logout session
 @app.route('/logout')
