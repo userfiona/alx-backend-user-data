a96d0445e71948aefce5b0cd86edf1275b445dc3
userfiona <fionaramaesela.com>
2024-02-21 13:02:14 +0000

    how to add authentication in to your app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -96,10 +96,4 @@ def update_password() -> tuple:
 
     try:
         AUTH.update_password(reset_token, new_password)
-        return jsonify({"email": email, "message": "Password updated"}), 200
-    except ValueError:
-        abort(403)
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+        return
